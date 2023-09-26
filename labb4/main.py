867d802f18be19f1d23f6bc711fba1be3a5afca8
Fr0dr1k <77593162+Fr0dr1k@users.noreply.github.com>
2023-09-26 14:51:38 +0200

    By Fredrik

--- labb4/main.py
+++ labb4/main.py
@@ -1,9 +1,10 @@
-import random,urllib.request,ssl,string
+import random,urllib.request,ssl,string,pickle
 
 ssl._create_default_https_context = ssl._create_unverified_context
 #url_without_emails = "https://user.it.uu.se/~joachim/"
 url_with_emails = "https://www.math.uu.se/institutionen/personal/"
 
+EMAIL_CHARS = string.ascii_letters + string.digits + '.'
 
 def get_source_code(url):
     file_object = urllib.request.urlopen(url)
@@ -19,9 +20,22 @@ def is_valid_email(email_address, at_symbol):
     #Nu så säger vi att ett namn kan vara alla typer av texter som innehåller bara bokstäver, siffror och punkter
     #En webbadress säger vi nu är alla godkänd webbadresser så ett domännamn.toppdomän tex gmail.com
     try:
+        with open("top_domains.txt", "rb") as file:
+            valid_top_domains = pickle.load(file)
+
         email_address = email_address.split(at_symbol)
         if not len(email_address) == 2:
             return False
+
+        if len(email_address[0]) < 1 or len(email_address[1]) < 1:
+            return False
+
+        if "." not in email_address[1]:
+            return False
+
+        if email_address[1][email_address[1].rfind(".")+1:].upper() not in valid_top_domains:
+            return False
+
         for i in email_address[0]:
             if i not in EMAIL_CHARS:
                 return False
@@ -37,57 +51,62 @@ def is_valid_email(email_address, at_symbol):
         return False
 
 
-
-EMAIL_CHARS = string.ascii_letters + string.digits + '.'
-
-
 def find_name_start(text, at_index):
     first_index = 0
     for index in range(at_index-1, -1, -1): #baklänges!
         if text[index] not in EMAIL_CHARS:
+
             first_index = index + 1
             break
     return first_index
 
 
 def find_email_end(text, at_index):
-    last_index = len(text)-1
-    print(text[at_index])
-    for i in range(at_index, len(text)-1):
-        if i not in EMAIL_CHARS:
+    last_index = len(text)
+    for i in range(at_index+1, len(text)-1):
+        if text[i] not in EMAIL_CHARS:
             last_index = i
             break
     return last_index
 
 
-def find_first_email(text):
-    at_index = text.find('@')
-    if at_index < 0:
-        return None
-    name_start = find_name_start(text, at_index)
-    if name_start == at_index:
-        return None
-    return text[name_start: at_index + 1]
-
-
 def get_all_emails(source_code_string, at_symbol):
-    all_potential_email = remove_html_code(source_code_string, at_symbol)
-    all_emails = set()
+    try:
+        all_potential_email = remove_html_code(source_code_string, at_symbol)
+        all_emails = set()
 
-    for emails in all_potential_email:
-        emails = [i for i in emails.split() if is_valid_email(i, at_symbol)]
-        all_emails.update(emails)
+        for emails in all_potential_email:
+            #emails = [i for i in emails.split() if is_valid_email(i, at_symbol)]
+            while at_symbol in emails:
+                at_index = emails.find(at_symbol)
+                start_index = find_name_start(emails,at_index)
+                end_index = find_email_end(emails,at_index)
+                new_email = emails[start_index:end_index]
+                emails = emails[end_index:]
 
-    return all_emails
+                if is_valid_email(new_email,at_symbol):
+                    all_emails.add(new_email)
+
+
+            #all_emails.update(emails)
+
+        return all_emails
+    except:
+        print("Something is wrong I can feel it -Eminem")
 
 
 def remove_html_code(source_code_string, at_symbol):
     all_lines = set()
+
     for line in source_code_string.split("\n"):
         remove_start_stop = []
 
         if at_symbol not in line:
             continue
+
+        if "mailto:" in line:
+            index_of_mailto = line.find("mailto:")
+            all_lines.add(line[index_of_mailto:line.find("\"", index_of_mailto)])
 
         for i in range(len(line)):
             if line[i] == "<":
@@ -96,6 +115,7 @@ def remove_html_code(source_code_string, at_symbol):
                 remove_start_stop.append(i)
 
         for i in range(len(remove_start_stop)-1,-1,-2):
+
             line = line[0:remove_start_stop[i-1]]+line[remove_start_stop[i]+1:]
 
         if at_symbol in line:
@@ -118,16 +138,22 @@ def find_links(source_code_string):
     return all_links
 
 
+def emails_from_url(url):
+    content = get_source_code(url)
+    all_emails = get_all_emails(content,"@")
+
+    for i in all_emails:
+        print(i)
+
 if __name__ == '__main__':
     print("Running")
     file = open("test.txt", "r", encoding="utf8")
-    test = file.read()
-    find_links(test)
+    #test = file.read()
+    #find_links(test)
     #print(*get_all_emails(test, "@"), sep="\n")
+    #print(get_all_emails("\\ntest@hej.se","@"))
+    print(get_all_emails(1,"@"))
+    #print(get_all_emails(get_source_code("http://www.it.uu.se/katalog/bylastname"),"@"))
     #<a HREF="http://www.uu.se">Uppsala Universitet</a>
 
-    print("Done runing")
-
-    #Att fixa
-    #Problem med specialtecken som start och slut på mail
-    #Fixa så att den tar upp mail adreser som ligger som länkar
+    print("Done running")
---
+++ labb4/testing_testing.py
@@ -0,0 +1,20 @@
+import main as m
+
+def test_get_mail_from_string():
+    expected_results = {"test luckass@gmail.com test ": {"luckass@gmail.com"},
+                        "fredrik@ff2.se": {"fredrik@ff2.se"},
+                        "test@": set(),
+                        "mailto:fredrik@ff2.se": {"fredrik@ff2.se"},
+                        "t    e££@öööö.#\n#": set(),
+                        "fredrik@ff2.aqv greger@test.se": {"greger@test.se"},
+                        "hej@test.com  @ ## £$@$ test@jag.se": {"hej@test.com", "test@jag.se"}}
+
+    count = 0
+    passed = 0
+    for i in expected_results:
+        count += 1
+        print(str(count)+".", m.get_all_emails(i,"@"), "should be",expected_results[i])
+        if m.get_all_emails(i,"@")==expected_results[i]:
+            passed += 1
+    print(str(passed)+"/"+str(len(expected_results))+" tests passed")
+
